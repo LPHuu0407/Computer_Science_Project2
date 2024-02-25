30fa9df4fbc549cbad7396a3b17dfaa344019c67
LE PHUOC HUU <lphuu04072003@gmail.com>
2024-02-25 16:47:18 +0700

    Commit files

---
+++ PRACTICE/Classification_Text_Demo.py
@@ -0,0 +1,19 @@
+import os
+import random
+import string
+from nltk import word_tokenize
+from collections import defaultdict
+from nltk import FeatDict
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+import pickle
+
+def create_data_set():
+    with open('data.txt', 'w', encoding='utf8') as outfile:
+        
+
+if __name__ == '__main__':
+    create_data_set()
