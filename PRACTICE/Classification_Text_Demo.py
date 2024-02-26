870869a448cb2974b8417cafcaf95ac7c0aad9c3
LE PHUOC HUU <lphuu04072003@gmail.com>
2024-02-26 12:18:14 +0700

    Update files

--- PRACTICE/Classification_Text_Demo.py
+++ PRACTICE/Classification_Text_Demo.py
@@ -11,20 +11,49 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 import pickle
 
-BASE_DIR = ''
-LABELS = ['business', 'entertainment', 'politics', 'sport', 'tech']
+# BASE_DIR = 'D:\\MYLEARNING\\THE_JOURNEY_IV\\COMPUTER_SCIENCE_PROJECT_2\\PRACTICE\\bbc'
+# LABELS = ['business', 'entertainment', 'politics', 'sport', 'tech']
 
-def create_data_set():
-    with open('data.txt', 'w', encoding='utf8') as outfile:
-        for label in LABELS:
-            dir = '%s/%s' % (BASE_DIR, label)
-            for filename in os.listdir(dir):
-                fullfilename = '%s/%s' % (dir, filename)
-                print(fullfilename)
-                with open(fullfilename, 'rb') as file:
-                    text = file.read().decode(errors= 'replace').replace('\n', '')
-                    outfile.write('%\t%s\t%s\n' % (label, filename, text))
+
+# def create_data_set():
+#     with open('data.txt', 'w', encoding='utf8') as outfile:
+#         for label in LABELS:
+#             dir = '%s/%s' % (BASE_DIR, label)
+#             for filename in os.listdir(dir):
+#                 fullfilename = '%s/%s' % (dir, filename)
+#                 print(fullfilename)
+#                 with open(fullfilename, 'rb') as file:
+#                     text = file.read().decode(errors= 'replace').replace('\n', '')
+#                     outfile.write('%\t%s\t%s\n' % (label, filename, text))
+
+def setup_docs():
+    docs = [] # (label, text)
+    with open('D:\MYLEARNING\THE_JOURNEY_IV\COMPUTER_SCIENCE_PROJECT_2\PRACTICE\data.txt', 'r', encoding='utf8') as datafile:
+        for row in datafile:
+            parts = row.split('\t')
+            doc = ( parts[0], parts[2].strip() )
+            docs.append(doc)
+        return docs
+
+def print_frequency_dist(docs):
+    tokens = defaultdict(list)
+    # lets make a giant list of all the words for each category
+    for doc in docs:
+        doc_label = doc[0]
+        doc_text = doc[1]
+        doc_tokens = word_tokenize(doc_text)
+        tokens[doc_label].extend(doc_tokens)
+    for category_label, category_tokens in tokens.items():
+        print(category_label)
+        fd = FreqDist(category_tokens)
+        print(fd.most_common(20))
 
 
 if __name__ == '__main__':
-    create_data_set()
+    #create_data_set()
+    docs = setup_docs()
+    print_frequency_dist(docs)
+    #train_classifier(docs)
+    # new_doc = "Google showed..."
+    #classify(new_doc)
+    print("Done")
