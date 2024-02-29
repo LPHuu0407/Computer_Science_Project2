44ed4459e856ef94b96b38a9ca3a01a9beaf8313
LE PHUOC HUU <lphuu04072003@gmail.com>
2024-02-29 10:11:47 +0700

    Updata files

--- PRACTICE/Classification_Text_Demo.py
+++ PRACTICE/Classification_Text_Demo.py
@@ -11,6 +11,10 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 import pickle
 
+stop_words = set(stopwords.words('english'))
+stop_words.add('said')
+stop_words.add('mr')
+
 # BASE_DIR = 'D:\\MYLEARNING\\THE_JOURNEY_IV\\COMPUTER_SCIENCE_PROJECT_2\\PRACTICE\\bbc'
 # LABELS = ['business', 'entertainment', 'politics', 'sport', 'tech']
 
@@ -41,6 +45,13 @@ def clean_text(text):
     # convert to lower case
     text = text.lower()
     return text
+ 
+def get_tokens(text):
+    #get individual words
+    tokens = word_tokenize(text)
+    # remove common words that are useless
+    tokens = [t for t in tokens if not t in stop_words]
+    return tokens
 
 def print_frequency_dist(docs):
     tokens = defaultdict(list)
@@ -48,20 +59,68 @@ def print_frequency_dist(docs):
     # hãy tạo một danh sách khổng lồ gồm tất cả các từ cho mỗi danh mục
     for doc in docs:
         doc_label = doc[0]
-        doc_text = doc[1]
-        doc_tokens = word_tokenize(doc_text)
+        #doc_text = doc[1] sau khi đã tìn ra các từ xuất hiện nhiều nhất, đến bước clean text #1
+        doc_text = clean_text(doc[1]) # clean text, xóa bỏ các dấu câu #2
+        #doc_tokens = word_tokenize(doc_text) #3
+        doc_tokens =get_tokens(doc_text) #4
         tokens[doc_label].extend(doc_tokens)
+
     for category_label, category_tokens in tokens.items():
         print(category_label)
         fd = FreqDist(category_tokens)
         print(fd.most_common(20))
 
+def get_splits(docs):
+    # scramb docs
+    random.shuffle(docs)
+    X_train = []# training documents
+    y_train = []# corresponding training labels
+    X_test = []# test documents
+    y_test = []# corresponding test labels
+    pivot = int(.80 * len(docs))
+    for i in range(0, pivot):
+        X_train.append(docs[i][1])
+        y_train.append(docs[i][0])
+    for i in range(pivot, len(docs)):
+        X_test.append(docs[i][1])
+        y_test.append(docs[i][0])
+    return X_train, X_test, y_train, y_test
+
+def evaluate_classifier(title, classifier, vectorizer, X_test, y_test):
+    X_test_tfidf = vectorizer.transform(X_test)
+    y_pred = classifier.predict(X_test_tfidf)
+
+    precision = metrics.precision_score(y_test, y_pred, average='macro')
+    recall = metrics.recall_score(y_test, y_pred, average='macro')
+    f1 = metrics.f1_score(y_test, y_pred, average='macro')
+
+    print("%s\t%f\t%f\t%f\n" % (title, precision, recall, f1))
+
+def train_classifier(docs):
+    X_train, X_test, y_train, y_test = get_splits(docs)
+    # the object that turns text into vector
+    vectorizer = CountVectorizer(stop_words='english', ngram_range=(1, 3), min_df=3, analyzer='word')
+    # create doc-term matrix
+    dtm = vectorizer.fit_transform(X_train)
+    # train Naive Bayes classifier
+    naive_bayes_classifier = MultinomialNB().fit(dtm, y_train)
+    
+    evaluate_classifier("Naive Bayes\tTRAI\t", naive_bayes_classifier, vectorizer, X_train, y_train)
+    evaluate_classifier("Naive Bayes\tTEST\t", naive_bayes_classifier, vectorizer, X_test, y_test)
+
+    # # store the classifier 
+    # clf_filename = 'naive_bayes_classifier.pkl'
+    # pickle.dump(naive_bayes_classifier, open(clf_filename, 'wb'))
+
+    # # also store the vectorizer so we transform new data
+    # vec_filename = 'count_vectorizer.pkl'
+    # pickle.dump(vectorizer, open(vec_filename, 'wb'))
 
 if __name__ == '__main__':
     #create_data_set()
     docs = setup_docs()
-    print_frequency_dist(docs)
-    #train_classifier(docs)
+    #print_frequency_dist(docs)
+    train_classifier(docs)
     # new_doc = "Google showed..."
     #classify(new_doc)
     print("Done")
